2995314f604e1b5882cd4e63038efad0a0181ae4
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-14 12:13:04 -0500

    Create restriction middleware that validate the user

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+            if (err) {
+                res.status(401).json({ message: "Invalid Credentials." })
+            } else {
+                req.decodedToken = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(400).json({ message: "This page is only available to authorized users." })
+    }
+};
